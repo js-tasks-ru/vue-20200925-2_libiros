a25d6b4cfb84b073e971a19e159ad85871f2b63f
Michael Fedotov <libiros@gmail.com>
2021-02-19 20:09:48 +0300

    add solution for MeetupPageComponent

--- 02-components/02-MeetupPageComponent/MeetupAgendaItem.js
+++ 02-components/02-MeetupPageComponent/MeetupAgendaItem.js
@@ -1,23 +1,38 @@
+import { agendaItemTitles, agendaItemIcons } from './data.js';
+
 export const MeetupAgendaItem = {
   name: 'MeetupAgendaItem',
 
   template: `<div class="meetup-agenda__item">
       <div class="meetup-agenda__item-col">
-        <img class="icon" alt="icon" src="/assets/icons/icon-cal-sm.svg" />
+        <img class="icon" alt="icon" :src="'/assets/icons/icon-'+icon+'.svg'" />
       </div>
-      <div class="meetup-agenda__item-col">00:00 - 00:00</div>
+      <div class="meetup-agenda__item-col">{{agendaItem.startsAt}} - {{agendaItem.endsAt}}</div>
       <div class="meetup-agenda__item-col">
-        <h5 class="meetup-agenda__title">Заголовок</h5>
-        <p>
-          <span>Докладчик</span>
-          <span class="meetup-agenda__dot"></span>
-          <span class="meetup-agenda__lang">ru</span>
+        <h5 v-if="agendaItem.title" class="meetup-agenda__title">{{agendaItem.title}}</h5>
+        <h5 class="meetup-agenda__title" v-else>{{title}}</h5>
+        <p v-if="(agendaItem.speaker || agendaItem.language)">
+          <span>{{agendaItem.speaker}}</span>
+          <span v-if="agendaItem.speaker" class="meetup-agenda__dot"></span>
+          <span v-if="agendaItem.language"class="meetup-agenda__lang">{{agendaItem.language}}</span>
         </p>
-        <p>Описание</p>
+        <p v-if="agendaItem.description">{{agendaItem.description}}</p>
       </div>
     </div>`,
 
-  // props
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true
+    }
+  },
 
-  // Возможно, тут потребуется computed
+  computed: {
+    title: function() {
+      return agendaItemTitles[this.agendaItem.type];
+    },
+    icon: function() {
+      return agendaItemIcons[this.agendaItem.type];
+    }
+  }
 };
--- 02-components/02-MeetupPageComponent/MeetupInfo.js
+++ 02-components/02-MeetupPageComponent/MeetupInfo.js
@@ -2,19 +2,48 @@ export const MeetupInfo = {
   template: `<ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="time">{{timeRU}}</time>
       </li>
     </ul>`,
 
-  // props
 
-  // computed
+  data() {
+    return {
+      dateOptions: { year: 'numeric', month: 'long', day: 'numeric' }
+    }
+  },
+
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    place: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    date: {
+      type: Date,
+      required: true
+    }
+  },
+
+  computed: {
+    time: function() {
+      return (Number.isInteger(this.date)) ? new Date(this.date).toISOString().substr(0, 10) : '';
+    },
+    timeRU: function() {
+      return new Date(this.date).toLocaleString('en-En',this.dateOptions)
+    }
+  }
 };
