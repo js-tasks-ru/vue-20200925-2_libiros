b2cfe1403eb8e279ec510e6fcfff6ed46ca864af
Михаил Федотов <libiros@MacBook-Air-lib.local>
2021-06-20 20:27:28 +0300

    Solution for PageWithQuery

--- 04-SPA/06-Query/views/PageWithQuery.vue
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="container">
-    <meetups-view />
+    <meetups-view v-bind.sync="queryParams" />
   </div>
 </template>
 
 <script>
 import MeetupsView from '../components/MeetupsView';
 
+const defaultParams = {
+  view: 'list',
+  date: 'all',
+  participation: 'all',
+  search: '',
+};
+
 export default {
   name: 'PageWithQuery',
   components: { MeetupsView },
+  data() {
+    return {
+      queryParams: { ...defaultParams },
+    };
+  },
+  watch: {
+    $route: {
+      handler(val) {
+        this.queryParams = Object.assign({}, defaultParams, val.query);
+      },
+      deep: true,
+      immediate: true,
+    },
+    queryParams: {
+      deep: true,
+      handler(v) {
+        const route = { query: Object.assign({}, v) };
+
+        for (let item in route.query) {
+          if (route.query[item] === defaultParams[item]) {
+            delete route.query[item];
+          }
+        }
+        // PUSH выбран затем ,чтобы можно было откатываться назад кнопкой "назад" в браузере
+        this.$router.push(route).catch((err) => {
+          if (err.name !== 'NavigationDuplicated') {
+            throw err;
+          }
+        });
+      },
+    },
+  },
 };
 </script>
 
