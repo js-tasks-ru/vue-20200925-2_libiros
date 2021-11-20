2e0971046a47e99191f679ae3eb8239291b5e7ca
test <libiros@MacBook-Air-lib.local>
2021-11-20 17:43:18 +0300

    solution for deep-pagination

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,29 @@
 <script>
 export default {
   name: 'NodesPagination',
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+  render: function (createElement) {
+    if (this.$slots.default) {
+      return createElement(
+        'div',
+        this.$slots.default.slice(
+          this.perPage * (this.page - 1),
+          this.perPage * this.page,
+        ),
+      );
+    } else {
+      return createElement('div');
+    }
+  },
 };
 </script>
 
