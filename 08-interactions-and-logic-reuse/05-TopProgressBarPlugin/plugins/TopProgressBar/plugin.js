284ed497139d66fef2a4735a792f47f3484fd69b
Michael Fedotov <libiros@gmail.com>
2022-05-15 16:52:16 +0300

    Solution for TopProgressBarPlugin

--- 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/plugin.js
+++ 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/plugin.js
@@ -1,4 +1,29 @@
-// import TopProgressBar from './index';
-// import TheTopProgressBar from './TheTopProgressBar';
+import TopProgressBar from './index';
+import TheTopProgressBar from './TheTopProgressBar';
 
-export default {};
+export default {
+  install(Vue, { router } = {}) {
+    if (!TopProgressBar.instance) {
+      const container = document.body.appendChild(
+        document.createElement('div'),
+      );
+      TopProgressBar.instance = new Vue(TheTopProgressBar).$mount(container);
+    }
+
+    if (router) {
+      router.beforeEach((to, from, next) => {
+        TopProgressBar.start();
+        next();
+      });
+      router.afterEach(() => {
+        TopProgressBar.finish();
+      });
+      router.onError((err) => {
+        TopProgressBar.fail();
+        throw err;
+      });
+    }
+
+    Vue.prototype.$progress = TopProgressBar;
+  },
+};
